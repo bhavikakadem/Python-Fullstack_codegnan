9ac2561ee64caf5a66730590944b580987512a9d
bhavikakadem <bhavikakadem@gmail.com>
2026-09-03 15:01:54 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,74 @@
+Python 3.10.11 (tags/v3.10.11:7d4cc5a, Apr  5 2023, 00:38:17) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #varialbes
+>>> print(5+4)
+9
+>>> a=45
+>>> b=18
+>>> print(a+b)
+63
+>>> d=45
+>>> print(d)
+45
+>>> e=17
+>>> print(e)
+17
+>>> print(E)
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    print(E)
+NameError: name 'E' is not defined. Did you mean: 'e'?
+>>> Name="bhavika"
+>>> print(name)
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    print(name)
+NameError: name 'name' is not defined. Did you mean: 'Name'?
+>>> print(Name)
+bhavika
+>>> a,b,c=(10,20,30)
+>>> print(a,b,c)
+10 20 30
+>>> fname="Bhavika"
+>>> lname="Kadem"
+>>> print(fname,lname)
+Bhavika Kadem
+>>> print(fname+" "+lname)
+Bhavika Kadem
+>>> a=45,b=18
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+>>> a,b,c=10,20,30
+>>> print(a,b,c)
+10 20 30
+>>> a,b,c,d=10,20,30,40,50
+Traceback (most recent call last):
+  File "<pyshell#23>", line 1, in <module>
+    a,b,c,d=10,20,30,40,50
+ValueError: too many values to unpack (expected 4)
+city="Vijayawada"
+print(city)
+Vijayawada
+country="India"
+print(country)
+India
+a2=10
+print(a2)
+10
+_a=10
+print(_a)
+10
+ a=45
+ 
+SyntaxError: unexpected indent
+1a=10
+SyntaxError: invalid decimal literal
+elif="Ninja"
+SyntaxError: invalid syntax
+first name="Bhavika"
+SyntaxError: invalid syntax
+del a
+print(a)
+Traceback (most recent call last):
+  File "<pyshell#37>", line 1, in <module>
+    print(a)
+NameError: name 'a' is not defined. Did you mean: 'a2'?
